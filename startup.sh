5712c4ef7e614a9f7fca52918e9fa265e96bc9c5
reyluar18 <reydelcastilloluarjr@gmail.com>
2024-07-20 05:02:52 +0430

    hardcode port

--- startup.sh
+++ startup.sh
@@ -6,6 +6,6 @@ screen -dmS websocket python /usr/local/sbin/websocket.py 8081
 screen -dmS socksovpn python /usr/local/sbin/socksovpn.py 80
 screen -dmS proxy python /usr/local/sbin/proxy.py 8010
 screen -dmS udpvpn /usr/bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 3
-screen -dmS slowdns-server ~/dnstt/dnstt-server/dnstt-server -udp :PORT_DNSTT_SERVER -privkey-file ~/dnstt/dnstt-server/server.key $(cat /root/ns.txt) 127.0.0.1:22
-screen -dmS slowdns-client ~/dnstt/dnstt-client/dnstt-client -dot 1.1.1.1:853 -pubkey-file ~/dnstt/dnstt-client/server.pub $(cat /root/ns.txt) 127.0.0.1:PORT_DNSTT_SSH_CLIENT
+screen -dmS slowdns-server ~/dnstt/dnstt-server/dnstt-server -udp :5300 -privkey-file ~/dnstt/dnstt-server/server.key $(cat /root/ns.txt) 127.0.0.1:22
+screen -dmS slowdns-client ~/dnstt/dnstt-client/dnstt-client -dot 1.1.1.1:853 -pubkey-file ~/dnstt/dnstt-client/server.pub $(cat /root/ns.txt) 127.0.0.1:2222
 screen -dmS webinfo php -S 0.0.0.0:5623 -t /root/.web/
