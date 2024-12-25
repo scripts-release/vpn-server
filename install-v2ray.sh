8dc98cfda61acf786666b2b35cdd64e705f0112d
reyluar18 <reydelcastilloluarjr@gmail.com>
2024-12-25 17:41:36 +0800

    hardcode the version to 24.9.16

--- install-v2ray.sh
+++ install-v2ray.sh
@@ -161,7 +161,8 @@ wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/scripts-rele
 # Install Xray #
 #==========#
 # / / Ambil Xray Core Version Terbaru
-latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+#latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
+latest_version="24.9.16"
 # / / Installation Xray Core
 xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
 # / / Ambil Xray Core Version Terbaru
